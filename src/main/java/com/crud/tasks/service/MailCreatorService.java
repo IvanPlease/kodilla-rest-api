5706a4ee4ced1bf46798d8ee437373b23321013e
QueIvan <szewczykjan2@gmail.com>
2020-08-08 16:56:24 +0200

    32.2

---
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -0,0 +1,22 @@
+package com.crud.tasks.service;
+
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class MailCreatorService {
+
+    private final TemplateEngine templateEngine;
+
+    public MailCreatorService(TemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+    }
+
+    public String buildTrelloCardEmail(String message){
+        Context context = new Context();
+        context.setVariable("message", message);
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+}
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -6,22 +6,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
 
 @Service
 @Slf4j
 public class SimpleEmailService {
     private final JavaMailSender javaMailSender;
+    private final MailCreatorService mailCreatorService;
 
     @Autowired
-    public SimpleEmailService(JavaMailSender javaMailSender) {
+    public SimpleEmailService(JavaMailSender javaMailSender, MailCreatorService mailCreatorService) {
         this.javaMailSender = javaMailSender;
+        this.mailCreatorService = mailCreatorService;
     }
 
     public void send(final Mail mail){
         log.info("Starting email preparation...");
         try{
-            SimpleMailMessage mailMessage = createMailMessage(mail);
+            MimeMessagePreparator mailMessage = createMimeMessage(mail);
             javaMailSender.send(mailMessage);
             log.info("Email has been sent.");
         }catch (MailException e){
@@ -29,14 +35,12 @@ public class SimpleEmailService {
         }
     }
 
-    private SimpleMailMessage createMailMessage(final Mail mail) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(mail.getMailTo());
-        mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mail.getMessage());
-        if(mail.getToCc() != null){
-            mailMessage.setCc(mail.getToCc());
-        }
-        return mailMessage;
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
     }
 }
